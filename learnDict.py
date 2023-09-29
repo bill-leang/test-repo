8697488e046e87b92ad49e59deb821e722be402c
bill-leang <65846978+bill-leang@users.noreply.github.com>
2023-09-29 11:29:11 +1000

    updated learnDict and learnPd.py

--- learnDict.py
+++ learnDict.py
@@ -1,1 +1,3 @@
-p
+mydict= {'sealand': 1e5, 'england': 25, 'spain': 35}
+del(mydict['sealand'])
+print(mydict)
--- learnPd.py
+++ learnPd.py
@@ -1,12 +1,29 @@
 import pandas as pd
+import numpy as np
+
+data = {1: 1, 2: 2, 3:3}
+ser = pd.Series(data)
+ser = ser.sort_index(ascending=False)
+print(ser)
+# ser2 = ser[ser>= 2]
+# myind = ser2.index
+# print(myind[-1])
+
+# # series = pd.Series([1, 0.9, 3, 0.89, 5])
+# series = pd.Series({'A': np.array([1, 0.9, 3, 0.89, 5])})
+# print(type(series))
+# # mask = series>1
+# print(series['A']> 1)
+
+
 # initialize data with null
-data= {'Col1':['C', 'A', 'C', 'D'],
-       'Col2':[1, 2, None , 4]}
-df = pd.DataFrame(data)
-df = df.set_index('Col1')
-df = df.sort_index()
-print(df)
-print(df.loc[['A','C','D'], 'Col2'])
+# data= {'Col1':['C', 'A', 'C', 'D'],
+#        'Col2':[1, 2, None , 4]}
+# df = pd.DataFrame(data)
+# df = df.set_index('Col1')
+# df = df.sort_index()
+# print(df)
+# print(df.loc[['A','C','D'], 'Col2'])
 # ser = df.loc[['A','C'], 'Col2']
 # print(pd.DataFrame(ser))
 # print(type(df.iloc[0:3, 0:1]))
